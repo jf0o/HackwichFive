d93229e644559adb435f9d32bcfaf9a37ebb7b16
CM Student <cmstudent@UHWO-ACM-16.local>
2019-03-14 10:47:30 -1000

    Stopped at Part 4.

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -13,9 +13,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var slider: UISlider!
     //create a variable that can hold the value of the slider
     var currentValue: Int = 0
+    
+    //create a variable to hold the target value
+    var targetValue: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //set currentValue to initial value of the slider
+        currentValue = lroundf(slider.value)
+        
+       //call startNewRound
+        startNewRound()
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,10 +36,16 @@ class ViewController: UIViewController {
         print("The value of the slider is:\(slider.value)")
         currentValue = lroundf(slider.value)
     }
+    //Part 3
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        lroundf(slider.value) = currentValue
+    }
     @IBAction func myGuessButtonPressed(_ sender: Any)
     {
         //5. New variable message that displays a message that includes currentValue
-        let message = "The value is: \(currentValue)"
+        let message = "The value is: \(currentValue)" + "\nThe target value is: \(targetValue)"
     
         //1. create alert view
         let alert = UIAlertController(title:"Hello World!", message: message, preferredStyle: .alert)
@@ -44,6 +58,8 @@ class ViewController: UIViewController {
         
         //4. present alerview on the screen
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
     }
 }
 
